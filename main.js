b5c52404d5d46b663f208fad22809a7d3dc7e375
Sanhith-7 <168118153+Sanhith-7@users.noreply.github.com>
2025-07-12 18:38:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+// Main entry point for the application
+console.log('Monad application is running!')
+
+// You can add your application logic here
+document.getElementById('root').innerHTML = '<h1>Welcome to Monad!</h1>' 
